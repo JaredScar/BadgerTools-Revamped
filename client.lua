03852b45454e3bfc0936a595959e61426e842b11
Jared <TheWolfBadger@gmail.com>
2019-11-14 09:50:06 -0500

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,352 @@
+-------------------
+--- BadgerTools ---
+------------------- 
+
+--- CODE ---
+
+-- START BadgerTools
+
+-- END BadgerTools
+
+function spectatePlayer(targetPed)
+	local playerPed = PlayerPedId() -- yourself
+	enable = true
+	
+	if targetPed == playerPed then enable = false end
+
+	if(enable)then
+		--local targetx,targety,targetz = table.unpack(GetEntityCoords(targetPed, false))
+		NetworkSetInSpectatorMode(true, targetPed)	
+		SetEntityInvincible(GetPlayerPed(-1), true) 
+	else
+		--local targetx,targety,targetz = table.unpack(GetEntityCoords(targetPed, false))
+		NetworkSetInSpectatorMode(false, targetPed)
+		SetEntityInvincible(GetPlayerPed(-1), false)
+	end
+end
+isSpectating = false;
+
+AddEventHandler('playerSpawned', function() 
+	TriggerServerEvent('BT:Server:PlayerSpawned')
+	NetworkSetTalkerProximity(proximity)
+end)
+
+RegisterNetEvent('BT:Client:Update')
+AddEventHandler('BT:Client:Update', function(tagHandler) 
+	activeTagsHandler = tagHandler
+	print("[BadgerTools] I got updated") -- DEBUG - Get rid of 
+end)
+
+RegisterNetEvent('BT:Client:UpdateColors')
+AddEventHandler('BT:Client:UpdateColors', function(colorHandle) 
+	colorPerms = colorHandle 
+end)
+
+RegisterNetEvent('BT:Client:SetTalkerProximity')
+AddEventHandler('BT:Client:SetTalkerProximity', function(val)
+	NetworkSetTalkerProximity(val + .0)
+	proximity = val + .0
+	print("[BadgerTools] Proximity was set to " .. tostring(val + .0)) 
+end)
+
+proximity = 15.0
+
+activeTagsHandler = {}
+colorPerms = {}
+
+prefix = '^9[^5BadgerTools^9] ^3';
+function sendMsg(msg) 
+	TriggerEvent('chatMessage', '', {255, 255, 255}, prefix .. '' .. msg)
+end
+
+RegisterCommand('proximity', function(source, args, rawCommand)
+	-- It's /proximity <distance>
+	if #args == 0 then 
+		-- They don't know how to use the command, tell them how 
+		sendMsg('^1ERROR: You must specify a distance. /proximity <distance>')
+	else 
+		local val = tonumber(args[1])
+		NetworkSetTalkerProximity(val + .0)
+		sendMsg('Your voice proximity has been set to ^5' .. args[1])
+	end  
+end)
+
+-- Spectate Cycle Controls --
+Citizen.CreateThread(function() 
+	while true do 
+		Citizen.Wait(0)
+		-- Runs every millisecond
+		-- LEFT ARROW = 174 RIGHT ARROW = 175
+		if isSpectating then 
+			-- They are spectating, check their controls and teleport them above the player 
+			if GetPlayerPed(spectatedUserClientID) == nil then 
+				-- Set them to another player to spectate 
+				if GetPlayersCountButSkipMe() >= 1 then 
+					local players = GetPlayersButSkipMyself()
+					local player = GetPlayerPed(players[1]) 
+					spectatedUserClientID = players[1]
+					spectatedUserServerID = GetPlayerServerId(spectatedUserClientID)
+					spectatePlayer(GetPlayerPed(spectatedUserClientID))
+					ShowNotification('~b~Spectating ~f~' .. GetPlayerName(spectatedUserClientID))
+					sendMsg('^5Spectating ^0' .. GetPlayerName(spectatedUserClientID))
+				else
+					-- Not enough players, spectate themselves 
+					ShowNotification("~r~Error: Not enough players to spectate")
+					spectatePlayer(GetPlayerPed(-1))
+					sendMsg('^1Error: Not enough players to spectate')
+					spectatedUserServerID = nil 
+					spectatedUserClientID = nil 
+					isSpectating = false 
+				end
+			end
+			local player = GetPlayerPed(-1)
+			local spectatedCoords = GetEntityCoords(GetPlayerPed(spectatedUserClientID))
+			--SetEntityCoords(player, spectatedCoords.x, spectatedCoords.y, spectatedCoords.z)
+			if IsControlJustReleased(0, 174) then 
+				-- Go backwards, spectatedUserClientID - 1
+				local players = GetPlayersButSkipMyself()
+				local index = getPlayerIndex(spectatedUserClientID)
+
+				index = index - 1;
+				if players[index] == nil then 
+					-- Can't go backwards anymore 
+					index = #players 
+				end
+				local newSpectate = players[index]
+				spectatedUserClientID = newSpectate 
+				spectatedUserServerID = GetPlayerServerId(newSpectate)
+				spectatePlayer(GetPlayerPed(spectatedUserClientID))
+				ShowNotification('~b~Spectating ~f~' .. GetPlayerName(spectatedUserClientID))
+				sendMsg('^5Spectating ^0' .. GetPlayerName(spectatedUserClientID))
+			elseif IsControlJustReleased(0, 175) then 
+				-- Go forwards, spectatedUserClientID + 1
+				local players = GetPlayersButSkipMyself()
+				local index = getPlayerIndex(spectatedUserClientID)
+				index = index + 1
+				if players[index] == nil then 
+					-- Can't go forward anymore 
+					index = 1 
+				end
+				local newSpectate = players[index]
+				spectatedUserClientID = tonumber(newSpectate) 
+				spectatedUserServerID = GetPlayerServerId(newSpectate)
+				spectatePlayer(GetPlayerPed(spectatedUserClientID))
+				ShowNotification('~b~Spectating ~f~' .. GetPlayerName(spectatedUserClientID))
+				sendMsg('^5Spectating ^0' .. GetPlayerName(spectatedUserClientID))
+			end
+		end 
+	end
+end)
+
+-- Voice Chat Handler --
+Citizen.CreateThread(function() 
+	while true do 
+		Citizen.Wait(0)
+		-- Runs every millisecond 
+		-- DrawText2("~f~" .. tag .. name, .50, 0.030 + (0.025*(counter)))
+		local players = GetPlayers()
+		local counter = 0;
+		for i=1, #players do 
+			local clientID = players[i]
+			local serverID = GetPlayerServerId(players[i])
+			if NetworkIsPlayerTalking(clientID) then 
+				-- They are talking, draw them on screen 
+				local playerName = GetPlayerName(clientID)
+				local playerCoords2 = GetEntityCoords(GetPlayerPed(clientID))
+				local playerCoords = GetEntityCoords(GetPlayerPed(-1))
+				if(GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, 
+					playerCoords.z, playerCoords2.x, playerCoords2.y, playerCoords2.z, true) <= proximity) then
+					-- They are in distance, draw them 
+					local hasColorPerms = colorPerms[serverID]
+					local activeTag = activeTagsHandler[serverID]
+					local colors = {"~b~", "~g~", "~y~", "~p~", "~c~", "~m~", "~u~", "~o~", "~w~", "~r~"}
+					if not hasColorPerms then 
+						for i = 1, #colors do
+							playerName = playerName:gsub(colors[i], "")
+						end
+					end
+					if activeTag ~= nil then 
+						DrawText2("~f~" .. activeTag .. playerName, .50, 0.005 + (0.025*(counter)))
+						counter = counter + 1
+					end
+				end
+			end
+		end
+	end
+end)
+
+spectatedUserServerID = nil 
+spectatedUserClientID = nil 
+
+function DrawText2(text, x, y)
+        SetTextFont(0)
+        SetTextProportional(1)
+        SetTextScale(0.0, 0.45)
+		SetTextJustification(1) -- Center Text
+		SetTextCentre(true)
+        SetTextDropshadow(1, 0, 0, 0, 255)
+        SetTextEdge(1, 0, 0, 0, 255)
+        SetTextDropShadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        AddTextComponentString(text)
+        DrawText(x, y)
+end
+
+RegisterNetEvent('BT:Client:SpectateID')
+AddEventHandler('BT:Client:SpectateID', function(id)
+	-- Spectate the specified ID 
+	if not isSpectating then 
+		-- They were not spectating, give them cycle spectate, but start from specified ID 
+		spectatedUserServerID = tonumber(id)
+		local players = GetPlayersButSkipMyself()
+		local found = false 
+		for i = 1, #players do 
+			local playerID = players[i] -- Their client side ID 
+			if spectatedUserServerID == GetPlayerServerId(playerID) then 
+				spectatedUserClientID = playerID
+				found = true 
+			end
+		end
+		if found then 
+			-- NEED TO SAVE THEIR LOCATION:
+			-- Save 
+			spectatePlayer(GetPlayerPed(spectatedUserClientID))
+			ShowNotification('~b~Spectating ~f~' .. GetPlayerName(spectatedUserClientID))
+			sendMsg('^5Spectating ^0' .. GetPlayerName(spectatedUserClientID))
+			isSpectating = true 
+		else 
+			-- Send message, can't find that ID 
+			sendMsg('^1ERROR: Cannot find the user with that ID')
+		end
+	else
+		-- They were spectating, just change the person they are spectating 
+		local serverID = tonumber(id)
+		local players = GetPlayersButSkipMyself()
+		local found = false 
+		for i = 1, #players do 
+			local playerID = players[i] -- Their client side ID 
+			if serverID == GetPlayerServerId(playerID) then 
+				spectatedUserClientID = playerID
+				spectatedUserServerID = serverID
+				found = true 
+			end
+		end
+		if found then 
+			TriggerServerEvent('BadgerTools:UserTag', false) -- Hide UserTag
+			spectatePlayer(GetPlayerPed(spectatedUserClientID))
+			ShowNotification('~b~Spectating ~f~' .. GetPlayerName(spectatedUserClientID))
+			sendMsg('^5Spectating ^0' .. GetPlayerName(spectatedUserClientID))
+			isSpectating = true 
+		else 
+			-- Send message, can't find that ID 
+			sendMsg('^1ERROR: Cannot find the user with that ID')
+		end
+	end 
+end)
+
+RegisterNetEvent('BT:Client:Spectate')
+AddEventHandler('BT:Client:Spectate', function()
+	-- Regular cycling spectate 
+	if not isSpectating then 
+		-- They are not spectating, spectate cycle start:
+		if GetPlayersCountButSkipMe() >= 1 then 
+			local players = GetPlayersButSkipMyself()
+			spectatedUserClientID = players[1]
+			spectatedUserServerID = GetPlayerServerId(spectatedUserClientID)
+			isSpectating = true 
+			-- NEED TO SAVE THEIR LOCATION:
+			-- Save 
+			spectatePlayer(GetPlayerPed(spectatedUserClientID))
+			ShowNotification('~b~Spectating ~f~' .. GetPlayerName(spectatedUserClientID))
+			sendMsg('^5Spectating ^0' .. GetPlayerName(spectatedUserClientID))
+			TriggerServerEvent('BadgerTools:UserTag', false) -- Hide UserTag
+		else
+			sendMsg('^1ERROR: Not enough players on to spectate') 
+		end 
+	else 
+		-- They were spectating, stop their spectate 
+		TriggerServerEvent('BadgerTools:UserTag', true) -- Show UserTag
+		ShowNotification("~g~Success: No longer spectating anyone!")
+		spectatePlayer(GetPlayerPed(-1))
+		sendMsg('^2Success: No longer spectating anyone!')
+		spectatedUserServerID = nil 
+		spectatedUserClientID = nil 
+		isSpectating = false 
+	end
+end)
+
+function has_value (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+function get_index (tab, val)
+	local counter = 1
+    for index, value in ipairs(tab) do
+        if value == val then
+            return counter
+        end
+		counter = counter + 1
+    end
+
+    return nil
+end
+
+function getPlayerIndex(id, skipMe) 
+	local counter = 0;
+	for _, i in ipairs(GetActivePlayers()) do
+        if NetworkIsPlayerActive(i) then
+        	if i ~= skipMe then 
+        		counter = counter + 1;
+			end
+			if i == id then
+				return counter;
+			end
+		end
+    end
+    return nil;
+end
+function GetPlayers()
+    local players = {}
+
+    for _, i in ipairs(GetActivePlayers()) do
+        if NetworkIsPlayerActive(i) then
+            table.insert(players, i)
+        end
+    end
+
+    return players
+end
+function GetPlayersButSkipMyself()
+    local players = {}
+
+    for _, i in ipairs(GetActivePlayers()) do
+        if NetworkIsPlayerActive(i) then
+			if i ~= PlayerId() then
+				table.insert(players, i)
+			end
+		end
+    end
+
+    return players
+end
+function GetPlayersCountButSkipMe()
+    local count = 0
+    for _, i in ipairs(GetActivePlayers()) do
+        if NetworkIsPlayerActive(i) then
+			if GetPlayerPed(i) ~= GetPlayerPed(-1) then
+				count = count + 1
+			end
+		end
+    end
+    return count
+end
+function ShowNotification(text)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawNotification(0,1)
+end
---
+++ server.lua
@@ -0,0 +1,178 @@
+-------------------
+--- BadgerTools ---
+-------------------
+
+--- Config ---
+roleList = {
+{0, "~y~Civilian | ~w~"}, -- 1
+{577635624618819593, '~r~Fire/EMS | ~w~'},
+{581881252907319369, '~g~LSPD | ~w~'},
+{577622764618383380, '~o~Sheriff | ~w~'},
+{506276895935954944, '~b~Highway | ~w~'},
+{609828128432586752, '~y~NG | ~w~'},
+{604137659836661760, '~p~Jr. Dev | ~w~'},
+{600800070618841098, '~b~E~g~L~y~I~o~T~p~E ~b~| '},
+{577661583497363456, "~g~Donator | "}, -- 3
+{577631197987995678, "~r~T-Mod | "}, -- 4
+{506211787214159872, "~r~Mod | "}, -- 5
+{506212543749029900, "~r~Admin | "}, -- 6
+{577966729981067305, "~p~Management | "}, -- 7
+{506212786481922058, "~o~Owner | "}, -- 8
+}
+
+function GetPlayersSkip(_source) 
+	local players = {}
+	for _, i in ipairs(GetPlayers()) do 
+		if not i == _source then
+			table.insert(players, i) 
+		end 
+	end
+	return players;
+end
+function GetPlayerWithHighestID(src)
+	local highest = nil;
+	for _, i in ipairs(GetPlayers()) do 
+		if i ~= src then 
+			if (highest == nil) or (tonumber(i) > tonumber(highest)) then 
+				highest = i;
+			end
+		end
+	end
+	return highest;
+end
+-- START BadgerTools
+RegisterCommand('kickRecent', function(source, args, rawCommand)
+	-- Kick the 3 most recent players 
+	if IsPlayerAceAllowed(source, 'BadgerTools.Commands.KickRecent') then 
+		if GetPlayerWithHighestID(source) ~= nil then 
+			DropPlayer(GetPlayerWithHighestID(source), "Server Care (Most recent ID)")
+		end 
+		if GetPlayerWithHighestID(source) ~= nil then 
+			DropPlayer(GetPlayerWithHighestID(source), "Server Care (Most recent ID)")
+		end 
+		if GetPlayerWithHighestID(source) ~= nil then 
+			DropPlayer(GetPlayerWithHighestID(source), "Server Care (Most recent ID)")
+		end 
+	end 
+end)
+
+prefix = '^9[^5BadgerTools^9] ^3';
+
+RegisterCommand('spectate', function(source, args, rawCommand)
+	if IsPlayerAceAllowed(source, 'BadgerTools.Commands.Spectate') then 
+		if #args > 0 then
+			-- /spectate 123 
+			local id = tonumber(args[1])
+			if GetPlayerIdentifiers(id)[2] ~= nil then 
+				TriggerClientEvent('BT:Client:SpectateID', source, id)
+			else 
+				TriggerClientEvent('chatMessage', source, prefix .. '^1ERROR: ^1That player cannot be found...')
+			end 
+		else
+			-- No args 
+			TriggerClientEvent('BT:Client:Spectate', source)
+		end
+	end -- End PlayerAceAllowed 
+end)
+
+-- Holds the VoiceTags of all users they are allowed to use 
+voiceTagHandler = {}
+
+-- Honds the active tags of all users 
+activeTagsHandler = {}
+
+-- List the voice tags you have access to: 
+RegisterCommand('voicetag', function(source, args, rawCommand)
+	if #args == 0 then 
+		-- They want the voicetags they have access to listed 
+		TriggerClientEvent('chatMessage', source, prefix .. 'You have access to the following voice-tags:')
+		local tags = voiceTagHandler[source]
+		for i=1, #tags do
+			TriggerClientEvent('chatMessage', source, '   ^3' .. tostring(i) .. ': ^5' .. tostring(tags[i]))
+		end  
+	else
+		-- They are selecting their voicetag  
+		local tagSelect = tonumber(args[1]);
+		local foundTag = false;
+		local tags = voiceTagHandler[source];
+		for i=1, #tags do
+			if i == tagSelect then 
+				TriggerClientEvent('chatMessage', source, prefix .. 'You have changed your voice-tag to: ^5' .. tags[i])
+				activeTagsHandler[source] = tags[i]
+				foundTag = true;
+			end
+		end
+		if not foundTag then 
+			TriggerClientEvent('chatMessage', source, prefix .. '^1ERROR: That voice-tag does not exist!')
+		else 
+			TriggerEvent('BT:Server:UpdateClients')
+		end
+	end 
+end)
+
+RegisterNetEvent('BT:Server:UpdateClients')
+AddEventHandler('BT:Server:UpdateClients', function()
+	TriggerClientEvent('BT:Client:Update', -1, activeTagsHandler)
+end)
+
+AddEventHandler('playerDropped', function (reason) 
+	-- Source is here, remove them from the dicts 
+	activeTagsHandler[source] = nil;
+	voiceTagHandler[source] = nil;
+	allowedColors[source] = nil;
+	TriggerEvent('BT:Server:UpdateClients')
+end)
+allowedColors = {}
+RegisterNetEvent('BT:Server:PlayerSpawned')
+AddEventHandler('BT:Server:PlayerSpawned', function()
+	-- Player joining the server
+	local src = source;
+	-- Code:
+	allowedColors[src] = false 
+	if IsPlayerAceAllowed(src, "BadgerTools.Colors") then 
+		allowedColors[src] = true 
+		TriggerClientEvent('BT:Client:UpdateColors', -1, allowedColors)
+	end
+	TriggerClientEvent('BT:Client:SetTalkerProximity', source, 15)
+	local identifierDiscord = nil 
+	for k, v in ipairs(GetPlayerIdentifiers(src)) do
+		if string.sub(v, 1, string.len("discord:")) == "discord:" then
+			identifierDiscord = v
+		end
+	end
+	local roleTags = {};
+	table.insert(roleTags, roleList[1][2])
+	activeTagsHandler[src] = roleList[1][2]
+	if identifierDiscord ~= nil then
+		-- Discord was found, get their roles 
+		local roleIDs = exports.discord_perms:GetRoles(src)
+		if roleIDs ~= false then 
+			for i=1, #roleList do 
+				for j=1, #roleIDs do 
+					if tostring(roleList[i][1]) == tostring(roleIDs[j]) then 
+						-- They have access to this role tag:
+						table.insert(roleTags, roleList[i][2]);
+						activeTagsHandler[src] = roleList[i][2];
+						print("[BadgerTools] Player " .. GetPlayerName(src) .. " has access to voicetag: " .. roleList[i][2])
+					end
+				end 
+			end
+		end
+	end
+	-- Set their tags they have access to: 
+	voiceTagHandler[src] = roleTags;
+
+	TriggerEvent('BT:Server:UpdateClients')
+end)
+
+RegisterNetEvent('BadgerTools:UserTag')
+AddEventHandler('BadgerTools:UserTag', function(bool)
+	if bool then
+		-- Show tag
+		exports.DiscordTagIDs:ShowUserTag(source)
+	else
+		-- Don't show tag
+		exports.DiscordTagIDs:HideUserTag(source)
+	end
+end)
+-- END BadgerTools
